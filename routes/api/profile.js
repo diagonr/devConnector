5ff58fb2419103736fe8d21cef8316ed6eb83734
Devin N <devinnguyen1994@yahoo.com>
2021-07-16 18:38:50 -0500

    DELETE profile education route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -279,4 +279,26 @@ router.put('/education', [auth,
     }
   })
 
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete profile education
+// @access  Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
