32713b9b259273f3f0a786113c2f5985ae4238ea
Devin N <devinnguyen1994@yahoo.com>
2021-09-28 16:01:47 -0500

    react links and async added

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export const Register = () => {
     const [formData, setFormdata] = useState( {
@@ -10,12 +11,12 @@ export const Register = () => {
 
     const { name, email, password, password2 } = formData;
     const onChange = e => setFormdata({ ...formData, [e.target.name]: e.target.value });
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2){
             console.log('Passwords do not match');
         } else {
-            console.log(formData);
+            console.log('Success');
         }
     }
 
@@ -57,7 +58,7 @@ export const Register = () => {
                 <input type="submit" className="btn btn-primary" value="Register" />
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <Link to="/login">Sign In</Link>
             </p>
         </Fragment>
     )
