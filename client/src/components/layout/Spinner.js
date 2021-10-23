5741d84b3e46a76905aa197b3b67ff5ecf62cb95
Devin N <devinnguyen1994@yahoo.com>
2021-10-22 21:47:05 -0500

    loading component added

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const Spinner = () => {
+  <Fragment>
+    <i
+      class='fa fa-spinner fa-spin fa-4x'
+      style={{ width: '200px', margin: 'auto', display: 'block' }}
+      alt='Loading...'
+    ></i>
+  </Fragment>;
+};
+
+export default Spinner;
